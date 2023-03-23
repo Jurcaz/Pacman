eea336cac86f3f6b3f845c374c68f6a5f0df03db
jmdin <jmdin@Juan>
2023-03-23 22:10:54 +0100

    230323 - dia trabajo casa 2

--- src/main/java/com/jpg/pacman/graphics/characters/Blinky.java
+++ src/main/java/com/jpg/pacman/graphics/characters/Blinky.java
@@ -11,11 +11,9 @@ import com.jpg.pacman.graphics.gameboard.MapElementEnum;
 public class Blinky extends Ghost {
 
 	//tiempo en milisegundos entre movimientos del fantasma
-	private int velocidadFantasma = 50;
+	private int velocidadFantasma = 500;
 	private DirectionEnum[] wayOut = {DirectionEnum.UP,DirectionEnum.RIGHT,DirectionEnum.UP,DirectionEnum.UP,DirectionEnum.LEFT};
-	private int out;
-	private boolean upBoolean, rightBoolean, downBoolean, leftBoolean;
-
+	
 	private final static Logger logger = LogManager.getLogger (Blinky.class);
 
 	/*
@@ -39,30 +37,12 @@ public class Blinky extends Ghost {
 			}
 
 			if(this.isOutCage()) {
-				int ghostX = (int) this.getX();
-				int ghostY = (int) this.getY();
-
-				int pacX = (int) this.tablero.getPacman().getX();
-				int pacY = (int) this.tablero.getPacman().getY();
-
-				System.out.println(findShortestPathLength(this.tablero.getMap(), ghostX, ghostY, pacX, pacY));
-
-				if(rightBoolean) {
-					this.currentDirection = DirectionEnum.RIGHT;
-				}else if(downBoolean) {
-					this.currentDirection = DirectionEnum.DOWN;
-				}else if(leftBoolean) {
-					this.currentDirection = DirectionEnum.LEFT;
-				}else if(upBoolean) {
-					this.currentDirection = DirectionEnum.UP;
-				}
-
+				findShortestPathLength(this.tablero.getMap(), ((int) this.getX()), ((int) this.getY()), ((int) this.tablero.getPacman().getX()), ((int) this.tablero.getPacman().getY()));
 				mover();
 			} else {
 				goOutCage();
 			}
 			
-			System.out.println("  --------------------Fin-de-turno--------------------  ");
 		}
 	}
 
@@ -82,90 +62,17 @@ public class Blinky extends Ghost {
 
 	@Override
 	protected void mover() {
-		if(currentDirectionAllowed()) go(this.currentDirection);
-	}
-
-	//----------------------------------------------------------------------------------------------------//
-
-    private boolean isSafe(boolean[][] visited, int x, int y) {
-        return moveAllowed(x, y) && !visited[x][y];
-    }
-
-    private int findShortestPath(MapElementEnum[][] mat, boolean[][] visited, int i, int j, int x, int y, int min_dist, int dist) {
-    	if (i == x && j == y) {
-    		if(dist<min_dist) {
-
-    			min_dist = dist;
-
-    			try {
-    				downBoolean = visited[((int) this.getX())][((int) this.getY())+1];
-				} catch (IndexOutOfBoundsException e) {
-					downBoolean = false;
-				}
-    			
-    			try {
-    				upBoolean = visited[((int) this.getX())][((int) this.getY())-1];
-				} catch (IndexOutOfBoundsException e) {
-					upBoolean = false;
-				}
-    			
-    			try {
-    				rightBoolean = visited[((int) this.getX())+1][((int) this.getY())];
-				} catch (IndexOutOfBoundsException e) {
-					rightBoolean = false;
-				}
-
-    			try {
-    				leftBoolean = visited[((int) this.getX())-1][((int) this.getY())];
-				} catch (IndexOutOfBoundsException e) {
-					leftBoolean = false;
-				}
-    			
-    		}
-
-    		return min_dist;
-    	}
-
-    	visited[i][j] = true;
-
-    	if (isSafe(visited, i + 1, j)) {min_dist = findShortestPath(mat, visited, i + 1, j, x, y, min_dist, dist + 1);}
-    	if (isSafe(visited, i, j + 1)) {min_dist = findShortestPath(mat, visited, i, j + 1, x, y, min_dist, dist + 1);}
-    	if (isSafe(visited, i - 1, j)) {min_dist = findShortestPath(mat, visited, i - 1, j, x, y, min_dist, dist + 1);}
-    	if (isSafe(visited, i, j - 1)) {min_dist = findShortestPath(mat, visited, i, j - 1, x, y, min_dist, dist + 1);}
-
-    	visited[i][j] = false;
-
-    	return min_dist;
-    }
-
-    private int findShortestPathLength(MapElementEnum[][] mat, int i, int j, int x, int y) {
-
-        int M = mat.length;
-        int N = mat[0].length;
-
-        int min_dist;
-
-        boolean[][] visited = new boolean[M][N];
-
-        min_dist = findShortestPath(mat, visited, i, j, x, y, Integer.MAX_VALUE, 0);
-
-       if (min_dist != Integer.MAX_VALUE) {
-    	   return min_dist;
-       }
-       return -1;
-    }
-
-	//----------------------------------------------------------------------------------------------------//
-
-	private boolean moveAllowed(float pXact, float pYact) {
-		if((pXact>=0 && pXact<this.getMapa().TAM_MAPA) && (pYact>=0 && pYact<this.getMapa().TAM_MAPA) &&
-				((this.getMapa().getMap()[(int)pYact][(int)pXact]==MapElementEnum.PILL) ||
-				 (this.getMapa().getMap()[(int)pYact][(int)pXact]==MapElementEnum.SUPERPILL) ||
-				 (this.getMapa().getMap()[(int)pYact][(int)pXact]==MapElementEnum.EMPTY)))
-			return true;
-		else {
-			return false;
+		if(rightBoolean) {
+			this.currentDirection = DirectionEnum.RIGHT;
+		}else if(downBoolean) {
+			this.currentDirection = DirectionEnum.DOWN;
+		}else if(leftBoolean) {
+			this.currentDirection = DirectionEnum.LEFT;
+		}else if(upBoolean) {
+			this.currentDirection = DirectionEnum.UP;
 		}
+		
+		if(currentDirectionAllowed()) go(this.currentDirection);
 	}
 
 	private void goOutCage() {
@@ -175,38 +82,6 @@ public class Blinky extends Ghost {
 			out++;
 		} catch (IndexOutOfBoundsException e) {
 			this.outCage = true;
-		}
-	}
-
-	private boolean currentDirectionAllowed() {
-		switch (this.currentDirection) {
-		case UP:
-			return moveAllowed(this.x, this.y-1);
-		case RIGHT:
-			return moveAllowed(this.x+1, this.y);
-		case DOWN:
-			return moveAllowed(this.x, this.y+1);
-		case LEFT:
-			return moveAllowed(this.x-1, this.y);
-		default:
-			return false;
-		}
-	}
-
-	private void go(DirectionEnum pDirection) {
-		switch (pDirection) {
-		case UP:
-			this.setY(this.y-1);
-			break;
-		case DOWN:
-			this.setY(this.y+1);
-			break;
-		case LEFT:
-			this.setX(this.x-1);
-			break;
-		case RIGHT:
-			this.setX(this.x+1);
-			break;
 		}
 	}
 
--- src/main/java/com/jpg/pacman/graphics/characters/Ghost.java
+++ src/main/java/com/jpg/pacman/graphics/characters/Ghost.java
@@ -5,6 +5,7 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 import com.jpg.pacman.graphics.gameboard.GameBoard;
+import com.jpg.pacman.graphics.gameboard.MapElementEnum;
 
 public abstract class Ghost extends Character implements Runnable {
 	protected boolean activo;
@@ -12,7 +13,8 @@ public abstract class Ghost extends Character implements Runnable {
 	protected int velocidad;
 	protected GameBoard gameboard;
 	protected boolean outCage = false;
-	protected DirectionEnum expectedDirection = null;
+	protected boolean upBoolean, rightBoolean, downBoolean, leftBoolean;
+	protected int out;
 
 	public Ghost(GameBoard gameBoard) {
 		super(gameBoard);
@@ -50,6 +52,8 @@ public abstract class Ghost extends Character implements Runnable {
 	public void muevePosicionInicial () {
 		super.muevePosicionInicial();
 		this.currentDirection = DirectionEnum.UP;
+		this.out = 0;
+		outCage = false;
 	}
 
 	@Override
@@ -84,7 +88,116 @@ public abstract class Ghost extends Character implements Runnable {
 		this.outCage = outCage;
 	}
 
+	protected void go(DirectionEnum pDirection) {
+		switch (pDirection) {
+		case UP:
+			this.setY(this.y-1);
+			break;
+		case DOWN:
+			this.setY(this.y+1);
+			break;
+		case LEFT:
+			this.setX(this.x-1);
+			break;
+		case RIGHT:
+			this.setX(this.x+1);
+			break;
+		}
+	}
+	
+	protected boolean currentDirectionAllowed() {
+		switch (this.currentDirection) {
+		case UP:
+			return moveAllowed(this.x, this.y-1);
+		case RIGHT:
+			return moveAllowed(this.x+1, this.y);
+		case DOWN:
+			return moveAllowed(this.x, this.y+1);
+		case LEFT:
+			return moveAllowed(this.x-1, this.y);
+		default:
+			return false;
+		}
+	}
+	
+	protected boolean moveAllowed(float pXact, float pYact) {
+		if((pXact>=0 && pXact<this.getMapa().TAM_MAPA) && (pYact>=0 && pYact<this.getMapa().TAM_MAPA) &&
+				((this.getMapa().getMap()[(int)pYact][(int)pXact]==MapElementEnum.PILL) ||
+				 (this.getMapa().getMap()[(int)pYact][(int)pXact]==MapElementEnum.SUPERPILL) ||
+				 (this.getMapa().getMap()[(int)pYact][(int)pXact]==MapElementEnum.EMPTY)))
+			return true;
+		else {
+			return false;
+		}
+	}
+	
+	protected boolean isSafe(boolean[][] visited, int x, int y) {
+        return moveAllowed(x, y) && !visited[x][y];
+    }
 
+    protected int findShortestPath(MapElementEnum[][] mat, boolean[][] visited, int i, int j, int x, int y, int min_dist, int dist) {
+    	if (i == x && j == y) {
+    		if(dist<min_dist) {
+
+    			min_dist = dist;
+
+    			try {
+    				downBoolean = visited[((int) this.getX())][((int) this.getY())+1];
+				} catch (IndexOutOfBoundsException e) {
+					downBoolean = false;
+				}
+    			
+    			try {
+    				upBoolean = visited[((int) this.getX())][((int) this.getY())-1];
+				} catch (IndexOutOfBoundsException e) {
+					upBoolean = false;
+				}
+    			
+    			try {
+    				rightBoolean = visited[((int) this.getX())+1][((int) this.getY())];
+				} catch (IndexOutOfBoundsException e) {
+					rightBoolean = false;
+				}
+
+    			try {
+    				leftBoolean = visited[((int) this.getX())-1][((int) this.getY())];
+				} catch (IndexOutOfBoundsException e) {
+					leftBoolean = false;
+				}
+    			
+    		}
+
+    		return min_dist;
+    	}
+
+    	visited[i][j] = true;
+
+    	if (isSafe(visited, i + 1, j)) {min_dist = findShortestPath(mat, visited, i + 1, j, x, y, min_dist, dist + 1);}
+    	if (isSafe(visited, i, j + 1)) {min_dist = findShortestPath(mat, visited, i, j + 1, x, y, min_dist, dist + 1);}
+    	if (isSafe(visited, i - 1, j)) {min_dist = findShortestPath(mat, visited, i - 1, j, x, y, min_dist, dist + 1);}
+    	if (isSafe(visited, i, j - 1)) {min_dist = findShortestPath(mat, visited, i, j - 1, x, y, min_dist, dist + 1);}
+
+    	visited[i][j] = false;
+
+    	return min_dist;
+    }
+
+    protected int findShortestPathLength(MapElementEnum[][] mat, int i, int j, int x, int y) {
+
+        int M = mat.length;
+        int N = mat[0].length;
+
+        int min_dist;
+
+        boolean[][] visited = new boolean[M][N];
+
+        min_dist = findShortestPath(mat, visited, i, j, x, y, Integer.MAX_VALUE, 0);
+
+       if (min_dist != Integer.MAX_VALUE) {
+    	   return min_dist;
+       }
+       return -1;
+    }
 
 
 }
